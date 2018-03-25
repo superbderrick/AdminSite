c5ec2308338f7982b3ea9cd619c1637604b9be60
derrick <kang.derrick@gmail.com>
2018-03-25 14:52:32 +0900

    basic template codes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Derrick Pratice' });
+  res.render('main', { title: 'Derrick Pratice' });
 });
 
 module.exports = router;

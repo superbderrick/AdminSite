25dfad58d743c075be9eb26119e83a5be21ca33e
derrick <kang.derrick@gmail.com>
2018-04-02 20:42:07 +0900

    improved functions

--- functions/index.js
+++ functions/index.js
@@ -22,3 +22,27 @@ exports.sanitizePost = functions.database.ref('/message').onWrite(event => {
 	return event.data.ref.set(post)
 })
 
+exports.emailEmployeeReport = functions.database.ref("/employee/{$eid}/reports/{$rid}")
+.onWrite(event => {
+  const eid = event.params.eid;
+  
+  const report = event.data.val().report;
+  const root = event.data.ref.root;
+  
+  const mgr_promise = root.child(`/employees/{$eid}/manager`).once("value");
+  
+  const then_promise = mgr_promisc.then(snap => {
+    const mgr_id = snap.val();
+    return root.child(`/employee/{$mrg_id}/email`).once("value");
+  }).then(snap => {
+    const email = snap.val();
+    sendReportEmail(email, report)
+  }).catch(reason => {
+    // Handle the error
+    console.log(reason);
+  });
+  
+  return then_promise2;
+  
+});
+
